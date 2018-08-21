adf3f3d8f3f126da9015d786c180169eb68e2012
darren mcgregor <darren.mcgregor@avanzac.co.uk>
2018-08-21 20:11:57 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,13 +6,17 @@ def display_board(board)
     puts " #{board[6]} | #{board[7]} | #{board[8]} "
   end
 
-def position_taken?(board, index)
-    if (board[index] == " ") || (board[index] == "") || (board[index] == nil)
-      return false
-    else
-      return true   #board[index] is not " " or "" or nil because there's an "X" or "O"
+  def input_to_index(user_input)
+       user_input.to_i - 1
+     end
+
+  def position_taken?(board, index)
+      if (board[index] == " ") || (board[index] == "") || (board[index] == nil)
+        return false
+      else
+        return true   #board[index] is not " " or "" or nil because there's an "X" or "O"
+      end
     end
-  end
 
 def valid_move?(board, index)
      if index.between?(0,8) && !position_taken?(board, index)
@@ -20,35 +24,18 @@ def valid_move?(board, index)
      end
    end
 
-def input_to_index(user_input)
-     user_input.to_i - 1
-   end
-
-def move(board, index, player)
+def move(board, index, player == "X")
      board[index] = player
    end
 
-   def turn_count(board)
-     counter = 0
-     board.each do |spaces|
-       if spaces == "X" || spaces == "O"
-         counter += 1
-       end
-     end
-     counter
-   end
-
-def current_player(board)
-  turn_count(board) % 2 == 0 ? "X" : "O"
-end
-
 def turn(board)
   puts "Please enter 1-9:"
-  
+  user_input = gets.chomp
   index = input_to_index(user_input)
-  if valid_move?(board, index)
-     move(board, index, current_player(board))
+  if valid_move?(board, index) == true
+     move(board, index)
+     display_board(board)
+   else
      turn(board)
    end
-  display_board(board)
-end
+ end
