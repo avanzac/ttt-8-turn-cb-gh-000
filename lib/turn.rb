d634e18a96165606d4ce6ef2fe8478d26e1f2df3
darren mcgregor <darren.mcgregor@avanzac.co.uk>
2018-08-21 20:01:44 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -43,11 +43,13 @@ def current_player(board)
 end
 
 def turn(board)
-  
+  puts "Please enter 1-9:"
+  user_input = gets.strip
   index = input_to_index(user_input)
   if valid_move?(board, index)
-    move(board,index,current_player(board))
-    turn(board)
-  end
+     move(board, index, current_player(board))
+     turn(board)
+   end
   display_board(board)
 end
+  
